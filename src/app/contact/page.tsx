fac246abb89ee8c6a1f62b0ff442bc0d12319667
sweatornella <ornellamoniquedioneepiebene@gmail.com>
2025-05-31 14:32:47 +0100

    contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,22 +5,20 @@ import AdditionalInfoSection from '@/components/honey/AdditionalInfoSection';
 
 export default function ContactPage() {
   return (
-    <main className="container mx-auto p-6">
-      {/* Titre principal */}
+    <main className="container mx-auto p-6 max-w-7xl">
       <h1 className="text-3xl font-bold mb-8 text-cyan-500 text-center md:text-left">Contact</h1>
 
-      {/* Grille principale */}
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-        {/* Colonne gauche - Formulaire */}
+        {/* Colonne gauche - Formulaire (2/3 de largeur) */}
         <div className="lg:col-span-2 space-y-8">
           <ContactForm />
           <SocialLinks />
         </div>
 
-        {/* Colonne droite - Informations */}
-        <div className="space-y-8">
+        {/* Colonne droite - Informations (1/3 de largeur) */}
+        <div className="lg:h-full lg:flex lg:flex-col">
           <ContactInfo />
-          <AdditionalInfoSection />
+          <AdditionalInfoSection className="flex-grow"/>
         </div>
       </div>
     </main>
